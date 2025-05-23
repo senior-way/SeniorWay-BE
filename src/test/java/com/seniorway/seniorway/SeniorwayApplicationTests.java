ec47a7d7e9e170ed0bc8446a921c3eef46290122
dlgkrwns213 <dlgkrwns213@naver.com>
2025-05-23 16:43:32 +0900

    Create Spring boot Project

---
+++ src/test/java/com/seniorway/seniorway/SeniorwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniorway.seniorway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
