f74a14676145c4e58c9f6834601637282882f6b4
dlgkrwns213 <dlgkrwns213@naver.com>
2025-05-27 14:45:14 +0900

    feat: userRepository interface 선언

---
+++ src/main/java/com/seniorway/seniorway/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.seniorway.seniorway.repository;
+
+import com.seniorway.seniorway.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Optional<User> findById(Long id);
+    Optional<User> findByUsername(String username);
+}
