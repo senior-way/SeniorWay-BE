af1df3b2ac7a5de03edfdd6db966ea991a47dd0e
yjs1228 <legoy1228@naver.com>
2025-06-12 16:30:57 +0900

    feat:trip 관련 repository 추가

---
+++ src/main/java/com/seniorway/seniorway/repository/trip/TripPlanRepository.java
@@ -0,0 +1,11 @@
+package com.seniorway.seniorway.repository.trip;
+
+import com.seniorway.seniorway.entity.trip.TripPlanEntity;
+import com.seniorway.seniorway.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TripPlanRepository extends JpaRepository<TripPlanEntity, Long> {
+    List<TripPlanEntity> findByUser(User user);
+}
