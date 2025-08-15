b9f135e7f28199e3b1ab51f944ad797e381972df
이학준 <dlgkrwns213@naver.com>
2025-08-16 00:19:20 +0900

    feat: @NoArgsConstructor 추가

--- src/main/java/com/seniorway/seniorway/dto/webSocket/LocationMessage.java
+++ src/main/java/com/seniorway/seniorway/dto/webSocket/LocationMessage.java
@@ -2,10 +2,12 @@ package com.seniorway.seniorway.dto.webSocket;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LocationMessage {
     private Long userId;
