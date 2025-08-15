136686a5837e7caaa781f16fd8dd9d04a460601c
이학준 <dlgkrwns213@naver.com>
2025-08-16 00:09:26 +0900

    feat: LocationController 선언

--- src/main/java/com/seniorway/seniorway/controller/webSocket/LocationController.java
+++ src/main/java/com/seniorway/seniorway/controller/webSocket/LocationController.java
@@ -1,9 +1,11 @@
 package com.seniorway.seniorway.controller.webSocket;
 
 import com.seniorway.seniorway.dto.webSocket.LocationMessage;
+import com.seniorway.seniorway.security.CustomUserDetails;
 import com.seniorway.seniorway.service.webSocket.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +15,9 @@ public class LocationController {
     private final LocationService locationService;
 
     @MessageMapping("/location")  // client -> server
-    public void receiveLocation(LocationMessage msg
-                                ) {
-        locationService.handleLocation(msg, authHeader);
+    public void receiveLocation(LocationMessage msg,
+                                @AuthenticationPrincipal CustomUserDetails userDetails) {
+        msg.setUserId(userDetails.getUserId());
+        locationService.handleLocation(msg, userDetails);
     }
 }
