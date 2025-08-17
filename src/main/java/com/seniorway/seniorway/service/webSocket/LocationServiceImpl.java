5ee497bc255fafa53b61914bc47c94d439d67ea6
이학준 <dlgkrwns213@naver.com>
2025-08-17 18:18:36 +0900

    feat: LocationService 에 redis 추가 기능 설정

--- src/main/java/com/seniorway/seniorway/service/webSocket/LocationServiceImpl.java
+++ src/main/java/com/seniorway/seniorway/service/webSocket/LocationServiceImpl.java
@@ -3,6 +3,7 @@ package com.seniorway.seniorway.service.webSocket;
 import com.seniorway.seniorway.dto.webSocket.LocationMessage;
 import com.seniorway.seniorway.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class LocationServiceImpl implements LocationService {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final StringRedisTemplate redisTemplate;
 
     @Override
     public void handleLocation(LocationMessage msg, CustomUserDetails userDetails) {
@@ -19,7 +21,16 @@ public class LocationServiceImpl implements LocationService {
     }
 
     private void saveLocation(LocationMessage msg) {
-        // TODO: Redis에 최근 위치 저장
+        // 1) 최신 위치 저장
+        String key = "location: " + msg.getUserId();
+        String value = msg.getLatitude() + "," + msg.getLongitude() + "," + System.currentTimeMillis();
+        redisTemplate.opsForValue().set(key, value);
+
+        // 2) 최근 위치 히스토리 (최대 10개)
+        String historyKey = "location:history:" + msg.getUserId();
+        redisTemplate.opsForList().leftPush(historyKey, value);
+        redisTemplate.opsForList().trim(historyKey, 0, 9);
+
         // TODO: DB에 위치 히스토리 저장
     }
 
