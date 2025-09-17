cbf355109593adc0ed5ceca852a77da197d4548d
이학준 <dlgkrwns213@naver.com>
2025-09-17 14:47:45 +0900

    feat: 사용자 역할 검증 로직 추가

--- src/main/java/com/seniorway/seniorway/service/user/UserGuardianLinkServiceImpl.java
+++ src/main/java/com/seniorway/seniorway/service/user/UserGuardianLinkServiceImpl.java
@@ -3,6 +3,7 @@ package com.seniorway.seniorway.service.user;
 import com.seniorway.seniorway.entity.user.User;
 import com.seniorway.seniorway.entity.user.UserGuardianLinkEntity;
 import com.seniorway.seniorway.enums.error.ErrorCode;
+import com.seniorway.seniorway.enums.user.Role;
 import com.seniorway.seniorway.exception.CustomException;
 import com.seniorway.seniorway.repository.user.UserGuardianLinkRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +21,16 @@ public class UserGuardianLinkServiceImpl implements UserGuardianLinkService {
         User user = userService.getUserByEmail(userEmail)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
+        if (user.getRole() != Role.USER) {
+            throw new CustomException(ErrorCode.INVALID_USER_ROLE);
+        }
+
         User guardian = userService.getUserById(guardianId)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        if (guardian.getRole() != Role.GUARDIANS) {
+            throw new CustomException(ErrorCode.INVALID_GUARDIAN_ROLE);
+        }
 
         UserGuardianLinkEntity link = userGuardianLinkRepository.save(UserGuardianLinkEntity.builder()
                         .user(user)
